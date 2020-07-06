59cfe1a7e5b17df199b0ec028b9840283160a723
Matheus Voltane Mendonça <65687322+theusvoltane@users.noreply.github.com>
2020-07-06 16:29:08 -0300

    Create Lab01.java
    Primeiro exercicio para usar os conhecimentos aprendidos.

---
+++ src/capitulo03/laboratorio/Lab01.java
@@ -0,0 +1,10 @@
+package capitulo03.laboratorio;
+
+public class Lab01 {
+	public static void main(String[] args) {
+		int valor1 = 10, valor2 = 20; 
+		int resultado = valor1 + valor2;
+		
+		System.out.println(valor1 + " + " + valor2 + " = " + resultado);
+	}
+}
