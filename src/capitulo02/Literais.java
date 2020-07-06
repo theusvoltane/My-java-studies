c480ac449127d67e15a9aec146d246e33706e085
Matheus Voltane Mendonça <65687322+theusvoltane@users.noreply.github.com>
2020-07-06 13:13:57 -0300

    Create Literais.java
    Diferentes formas de escrever o mesmo numero, binario, decimal, hexagonal entre outros.

---
+++ src/capitulo02/Literais.java
@@ -0,0 +1,13 @@
+package capitulo02;
+
+public class Literais {
+	public static void main(String[] args) {
+		int num01 = 100;
+		int num02 = 0x64;
+		int num03 = 0144;
+		int num04 = 0b1100100;
+		
+		System.out.println(num01 + "\n" + num02 + "\n" + num03 + "\n" + num04 );
+	}
+
+}
