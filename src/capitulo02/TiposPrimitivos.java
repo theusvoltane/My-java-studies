e98e4f31f6bd77de59be1a37bfbf70306b2af3ff
Matheus Voltane Mendonça <65687322+theusvoltane@users.noreply.github.com>
2020-07-06 13:15:19 -0300

    Create TiposPrimitivos.java
    Tipos primitivos de variaveis

---
+++ src/capitulo02/TiposPrimitivos.java
@@ -0,0 +1,46 @@
+package capitulo02;
+
+public class TiposPrimitivos {
+	public static void main(String[] args) {
+		//TIPOS PRIMITIVOS NUMERICOS INTEIROS
+		byte b = 10;
+		short s = 100;
+		int i = 100_000_000;
+		long l = 10560;
+		
+		//TIPOS PRIMITIVOS NUMERICOS REAIS
+		float f = 123.5f;
+		double d = 30500.88;
+		
+		//TIPOS PRIMITIVOS BOLEANOS
+		boolean teste01 = true;
+		boolean teste02 = false;
+		
+		//TIPOS PRIMITIVOS TEXTUAIS
+		char letra01 = 'J';
+		char letra02 = 74;
+		char letra03 = '\u004A';
+		
+		System.out.println("TIPOS PRIMITIVOS NUMERICOS INTEIROS");
+		System.out.println(b);
+		System.out.println(s);
+		System.out.println(i);
+		System.out.println(l);
+		System.out.println();
+		System.out.println("TIPOS PRIMITIVOS NUMERICOS REAIS");
+		System.out.println(f);
+		System.out.println(d);
+		System.out.println();
+		System.out.println("TIPOS PRIMITIVOS NUMERICOS BOLEANOS");
+		System.out.println(teste01);
+		System.out.println(teste02);
+		System.out.println();
+		System.out.println("TIPOS PRIMITIVOS TEXTUAIS");
+		System.out.println(letra01);
+		System.out.println(letra02);
+		System.out.println(letra03);
+		
+
+	}
+
+}
