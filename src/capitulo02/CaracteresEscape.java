7f1204c2444b534528e1133c57b4667d0ef3dd53
Matheus Voltane Mendonça <65687322+theusvoltane@users.noreply.github.com>
2020-07-06 13:09:50 -0300

    Create CaracteresEscape.java
    Caracteres para alterar uma String

---
+++ src/capitulo02/CaracteresEscape.java
@@ -0,0 +1,8 @@
+package capitulo02;
+
+public class CaracteresEscape {
+	public static void main(String[] args) {
+		String texto = "Java programmer";
+		System.out.println("Este texto esta em uma unica \"linha\"\n\tSaltei uma \"linha\" para o texto\n"+texto);
+	}
+}
