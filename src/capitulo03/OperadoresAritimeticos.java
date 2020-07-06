c4576abb73bd890a2d7c0b9aef02f30c2c9cf72c
Matheus Voltane Mendonça <65687322+theusvoltane@users.noreply.github.com>
2020-07-06 16:28:30 -0300

    Create OperadoresAritimeticos.java
    Exemplos de adição, subtração, divisão, modulo da divisão e multiplicação

---
+++ src/capitulo03/OperadoresAritimeticos.java
@@ -0,0 +1,23 @@
+package capitulo03;
+
+public class OperadoresAritimeticos {
+	public static void main(String[] args) {
+		int number01 = 10;//NUMBER RECEBE 125
+		int number02 = 3;
+		
+		//OPRADOR DE MULTIPLICAÇÃO
+		System.out.println(number01 * number02);
+		
+		//OPERADOR DE DIVISÃO
+		System.out.println(number01 / number02);
+		
+		//OPERADOR DE MÓDULO DA DIVIÃO
+		System.out.println(number01 % number02);
+		
+		//OPERADOR DE SOMA
+		System.out.println(number01 + number02);
+		
+		//OPERADOR DE SUBTRAÇÃO
+		System.out.println(number01 - number02);
+	}
+}
