b57458a079a4a07c281ddd126ffb4f659b44047d
Matheus Voltane Mendonça <65687322+theusvoltane@users.noreply.github.com>
2020-07-08 16:54:37 -0300

    Create Exercicio01.java
    Exercicio feito em aula, com if or else

---
+++ src/capitulo04/Exercicio01.java
@@ -0,0 +1,12 @@
+package capitulo04;
+
+public class Exercicio01 {
+	public static void main(String[] args) {
+		int idade = 26;
+		if (idade >= 18) {
+			System.out.println("Pode entrar na festa");
+		}else {
+			System.out.println("So pode entrar se estiver acompanhado de um adulto");
+		}
+	}
+}
